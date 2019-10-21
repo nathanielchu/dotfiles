9f274485fa3c1a7e75f8a12d07739bbebb709516
Nathaniel Chu <nathanielchu4@gmail.com>
2019-10-21 10:53:21 -0700

    Add bootstrap script for tmux to automatically install plugins

---
+++ tmux/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------
+# automated installation
+# ------------------------------------------------------------------
+
+# --- Flags --------------------------------------------------------
+set -o errexit          # Exit on most errors (see the manual)
+set -o errtrace         # Make sure any error trap is inherited
+set -o nounset          # Disallow expansion of unset variables
+set -o pipefail         # Use last non-zero exit code in a pipeline
+#set -o xtrace          # Trace the execution of the script (debug)
+
+# --- Locks --------------------------------------------------------
+SUBJECT=bootstrap_tmux
+LOCK_FILE=/tmp/"${SUBJECT}".lock
+if [ -f "${LOCK_FILE}" ]; then
+   echo "Script is already running"
+   exit
+fi
+
+trap "rm -f "${LOCK_FILE}"" EXIT
+touch "${LOCK_FILE}"
+
+# --- Body ---------------------------------------------------------
+__dir="$(cd "$(dirname "${BASH_SOURCE[${__b3bp_tmp_source_idx:-0}]}")" && pwd)"
+cd "${__dir}"
+
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+# ------------------------------------------------------------------
