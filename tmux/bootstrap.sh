9fdea7212f45223100e0dd463391e9a81c048bd6
Nathaniel Chu <nathanielchu4@gmail.com>
2020-01-29 13:13:01 -0800

    Move tmux tpm plugin manager install out of tmux conf into bash file

--- tmux/bootstrap.sh
+++ tmux/bootstrap.sh
@@ -29,8 +29,12 @@ INSTALLER="../install_package.sh"
 
 "${INSTALLER}" tmux
 
+if [ ! -d "~/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 tmux new-session -d
-tmux source ~/.tmux.conf
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+tmux source ~/.tmux.conf
 
 # ------------------------------------------------------------------
