990def0fe920bed38a4d3c147030ecfc97f9c1ba
Nathaniel Chu <nathanielchu4@gmail.com>
2020-11-02 11:31:11 -0800

    Fix version issues with installing vim, nodejs, yarn

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -25,11 +25,24 @@ touch "${LOCK_FILE}"
 __dir="$(cd "$(dirname "${BASH_SOURCE[${__b3bp_tmp_source_idx:-0}]}")" && pwd)"
 cd "${__dir}"
 
+# setup vim install
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+
+# Install node.js and yarn
+sudo apt update
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
+sudo apt update
+sudo apt -y install gcc g++ make
+sudo apt -y install nodejs
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
 INSTALLER="../install_package.sh"
 
 "${INSTALLER}" vim
-"${INSTALLER}" nodejs
-"${INSTALLER}" yarn
 "${INSTALLER}" ctags
 
 if ! [ -x "$(command -v rg)" ]; then
