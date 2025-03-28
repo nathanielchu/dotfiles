0cd9c1f98b65dc2fb55539855044b5c998b8ef47
Nathaniel Chu <nathanielchu4@gmail.com>
2025-03-27 23:14:57 -0700

    Handle installation errors in install_package.sh

--- install_package.sh
+++ install_package.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 if ! [ -x "$(command -v "${1}")" ]; then
-	echo "Installing "${1}""
-	sudo apt-get install "${1}"
+  echo "Installing ${1}"
+  sudo apt-get update # Update package list before installing
+  if ! sudo apt-get install -y "${1}"; then
+    echo "Error installing ${1}"
+    exit 1
+  fi
 else
-	echo ""${1}" already installed"
+  echo "${1} already installed"
 fi
